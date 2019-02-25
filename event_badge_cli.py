e9608a6a9cb36c77b5a1e466b453143a5f86ab21
Igorxp5 <rogixp5@gmail.com>
2019-02-25 09:09:00 -0300

    Criado comando 'view-psd-layers'
    Ver árvore de camadas de um arquivos PSD

--- event_badge_cli.py
+++ event_badge_cli.py
@@ -1,4 +1,48 @@
-"""CLI para gerar crachás de evento a partir de modelos em PSD."""
+"""CLI to generate event badges from PSD templates."""
 
 __author__ = 'Igor Fernandes'
 __version__ = '0.0.0'
+
+import click
+import treelib
+from psd_tools import PSDImage
+from collections.abc import Iterable
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    """CLI to generate event badges from PSD templates."""
+    pass
+
+
+@click.command('view-psd-layers')
+@click.argument('file', type=click.Path(exists=True, dir_okay=False))
+def viewPsdLayers(file):
+    """View layers tree of a PSD file."""
+    try:
+        psd = PSDImage.open(file)
+    except:
+        raise click.FileError(file, 'Expected a file in PSD format.')
+
+    def addLayersToTree(tree, layers):
+        if isinstance(layers, Iterable):
+            for layer in layers:
+                layerName = layer.name
+                layerId = layer.layer_id
+                layerType = layer.__class__.__name__
+                layer.treeAlias = layerId
+                exhibition = f'{layerName} (Id: {layerId}, Type={layerType})'
+                tree.create_node(exhibition, layerId, parent=layers.treeAlias)
+                addLayersToTree(tree, layer)
+    
+    psd.treeAlias = 'psd'
+    tree = treelib.Tree()
+    tree.create_node(f'PSD: {file}', psd.treeAlias)
+    addLayersToTree(tree, psd)
+    tree.show()
+
+cli.add_command(viewPsdLayers)
+
+if __name__ == '__main__':
+    cli()
