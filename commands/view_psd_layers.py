65f3d7cfb28f9be61176fb1658afe64f251601d4
Igorxp5 <rogixp5@gmail.com>
2019-02-25 11:39:22 -0300

    'view-psd-layers' - Corrigido ordem de exibição da árvore de camadas

--- commands/view_psd_layers.py
+++ commands/view_psd_layers.py
@@ -18,19 +18,43 @@ def view_psd_layers(file):
     except:
         raise click.FileError(file, 'Expected a file in PSD format.')
 
-    def add_layers_to_tree(tree, layers):
-        if isinstance(layers, Iterable):
-            for layer in layers:
-                layer_type = layer.__class__.__name__
-                layer.tree_alias = layer.layer_id
-                exhibition = (f'{layer.name}' +
-                              f'(Id: {layer.layer_id}, Type={layer_type})')
-                tree.create_node(exhibition, layer.layer_id,
-                                 parent=layers.tree_alias)
-                add_layers_to_tree(tree, layer)
-    
+    tree = get_psd_layers_tree(psd, f'PSD: {file}')
+    tree.show(key=lambda node: node.data, reverse=True)
+
+
+def get_psd_layers_tree(psd, root_name=None):
+    """Get ordered tree of layers from a PSD.
+
+    :param psd: PSD which will be extracted the layers.
+    :type tree: psd_tools.PSDImage or psd_tools.api.layers
+
+    :param root_name: Tree root name.
+    :type root_name: str
+    :default root_name: psd.name
+    """
+    root_name = psd.name if not root_name else root_name
     psd.tree_alias = 'psd'
     tree = treelib.Tree()
-    tree.create_node(f'PSD: {file}', psd.tree_alias)
+    tree.create_node(root_name, psd.tree_alias)
     add_layers_to_tree(tree, psd)
-    tree.show()
+    return tree
+
+
+def add_layers_to_tree(tree, layers):
+    """Add layers and sub-layers to a tree.
+
+    :param tree: Tree to which layers will be added.
+    :type tree: treelib.tree.Tree
+    :param layers: PSD root or layer that will be added to the tree.
+    :type layers: psd_tools.PSDImage or psd_tools.api.layers
+    """
+    if isinstance(layers, Iterable):
+        for index in range(len(layers) - 1, -1, -1):
+            layer = layers[index]
+            layer_type = layer.__class__.__name__
+            layer.tree_alias = layer.layer_id
+            exhibition = (f'{layer.name}' +
+                          f'(Id: {layer.layer_id}, Type={layer_type})')
+            tree.create_node(exhibition, layer.layer_id, data=index,
+                             parent=layers.tree_alias)
+            add_layers_to_tree(tree, layer)
